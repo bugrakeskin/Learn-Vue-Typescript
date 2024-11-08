3475aa10f5ef5d56f6c3960e55b69dd922ab0206
Buğra Keskin <bugrakeskin@gmail.com>
2024-11-08 15:44:32 +0300

    added tailwind to the project

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,25 @@
 <template>
-  <div>My Nuxt Page starter not compatible with NUXT4</div>
+  <div class="grid items-center space-xy-4 mt-4 p-2">
+    <input class="border" type="number" v-model="age" />
+
+    <div class="text-1xl">
+      Can you drive?
+      <span :class="canDrive === 'yes' ? 'text-green-600' : 'text-red-600'">{{
+        canDrive
+      }}</span>
+    </div>
+  </div>
 </template>
 
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+let age = ref(0);
+
+let canDrive = computed(() => {
+  return age.value >= 18 ? "yes" : "no";
+});
+/* setTimeout(() => {
+  let age = 34;
+}); */
+</script>
 
 <style></style>
